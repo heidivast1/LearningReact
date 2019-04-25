7e1b9e4be6a6885b76651c2774182d251fac0940
heidivast1 <43884561+heidivast1@users.noreply.github.com>
2019-04-25 10:35:13 -0400

    props and keys

--- app.js
+++ app.js
@@ -1,16 +1,75 @@
-const title = <h1>My First React Element!</h1>
+const players = [
+{
+  name: "Guil",
+  score: 50,
+  id: 1
+},
+{
+  name: "Treasure",
+  score: 85,
+  id: 2
+},
+{
+  name: "Ashley",
+  score: 95,
+  id: 3
+},
+{
+  name: "James",
+  score: 80,
+  id: 4
+}
+];
 
-const desc = <p>I just learned how to create a react Node and render it into the DOM.</p>
+const Header = (props) => {
+  console.log(props);
+    return (
+    <header>
+      <h1>{props.title}</h1>
+      <span className="stats">Players: {props.totalPlayers}</span>
+    </header>
+  );
+}
 
+const Player = (props) => {
+  return(
+    <div className="player">
+      <span className="player-name">{props.name}</span>
+      <Counter score={props.score}/>
+    </div>
+  );
+}
 
-const header = React.createElement(
-  'header',
-  null,
-  title,
-  desc
-);
+const Counter = (props) => {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement">-</button>
+      <span className="counter-score">{props.score}</span>
+      <button className="counter-action increment">+</button>
+    </div>
+  );
+}
+
+const App = (props) => {
+  return (
+    <div className="scoreboard">
+      <Header
+      title="Scoreboard"
+      totalPlayers={props.initialPlayers.length}
+      />
+
+      {props.initialPlayers.map( player =>
+        <Player
+        name={player.name}
+        score={player.score}
+        key={player.id.toString()}
+        />
+      )}
+    </div>
+  );
+}
 
 ReactDOM.render(
-  header,
+  <App initialPlayers={players}/>,
   document.getElementById('root')
 );
