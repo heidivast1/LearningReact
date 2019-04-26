5c4c5f23732061533cf9c12e81f27a832a1a1fd7
heidivast1 <43884561+heidivast1@users.noreply.github.com>
2019-04-26 11:52:17 -0400

    now interactive

--- app.js
+++ app.js
@@ -1,24 +1,5 @@
 const players = [
-{
-  name: "Guil",
-  score: 50,
-  id: 1
-},
-{
-  name: "Treasure",
-  score: 85,
-  id: 2
-},
-{
-  name: "Ashley",
-  score: 95,
-  id: 3
-},
-{
-  name: "James",
-  score: 80,
-  id: 4
-}
+
 ];
 
 const Header = (props) => {
@@ -46,34 +27,73 @@ class Counter extends React.Component {
       score: 0
     };
 
+  incrementScore = () => {
+    this.setState( prevState => ({
+      score: prevState.score + 1
+    }));
+  }
+
+  decrementScore = () => {
+    this.setState( prevState => ({
+      score: prevState.score - 1
+    }));
+  }
 
   render() {
     return (
       <div className="counter">
-        <button className="counter-action decrement">-</button>
+        <button className="counter-action decrement" onClick={this.decrementScore}>-</button>
         <span className="counter-score">{this.state.score}</span>
-        <button className="counter-action increment">+</button>
+        <button className="counter-action increment" onClick={ this.incrementScore}>+</button>
       </div>
     );
   }
 }
 
-const App = (props) => {
-  return (
-    <div className="scoreboard">
-      <Header
-      title="Scoreboard"
-      totalPlayers={props.initialPlayers.length}
-      />
+class App extends React.Component {
 
-      {props.initialPlayers.map( player =>
-        <Player
-        name={player.name}
-        key={player.id.toString()}
+  state = {
+    players: [
+      {
+        name: "Guil",
+        score: 50,
+        id: 1
+      },
+      {
+        name: "Treasure",
+        score: 85,
+        id: 2
+      },
+      {
+        name: "Ashley",
+        score: 95,
+        id: 3
+      },
+      {
+        name: "James",
+        score: 80,
+        id: 4
+      }
+    ]
+  };
+
+  render() {
+    return (
+      <div className="scoreboard">
+        <Header
+        title="Scoreboard"
+        totalPlayers={props.initialPlayers.length}
         />
-      )}
-    </div>
-  );
+
+        {props.initialPlayers.map( player =>
+          <Player
+          name={player.name}
+          key={player.id.toString()}
+          />
+        )}
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
